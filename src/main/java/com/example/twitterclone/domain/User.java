d7ab123c63657f3cac72d9c712c7710954a43ef4
mykytam <morar.nikita@gmail.com>
2020-03-02 17:45:36 +0200

    My Messages page added. Now you can also see each user's messages

--- src/main/java/com/example/twitterclone/domain/User.java
+++ src/main/java/com/example/twitterclone/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -34,6 +35,19 @@ public class User implements UserDetails {
     // получение сообщений, созданных пользователем
     @OneToMany (mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY) // при загрузке сообщения пользователя подгружаться не должны
     private Set<Message> messages;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
